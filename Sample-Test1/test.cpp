9489e12438fa106f1abe3bfa20ea54aa8cffb0eb
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-03 11:11:30 +0900

    [feature] 37p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -9,3 +9,9 @@ TEST(PriceFactors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, pf.of(1));
 }
+
+TEST(PriceFactors, Of2) {
+	PrimeFactor pf;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, pf.of(2));
+}
