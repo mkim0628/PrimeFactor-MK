bc6d09a97c0be440ffbfac46831e11332442f1c4
Myeong Woo Kim <mkim0628@gmail.com>
2024-07-03 11:40:30 +0900

    [feature] 54p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -35,3 +35,8 @@ TEST_F(PrimeFactorsFixture, Of6) {
 	expected = { 2, 3 };
 	EXPECT_EQ(expected, prime_factor.of(6));
 }
+
+TEST_F(PrimeFactorsFixture, Of9) {
+	expected = { 3, 3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
